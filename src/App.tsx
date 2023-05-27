5b13e0ba1c779fde928035790448aa7a9214bfb5
webader <alexey.derenko@gmail.com>
2023-05-27 19:49:59 +0300

    homework ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {userServices} from "./services/user.services";
 
+userServices.getAll().then(value => value.data).then(characters => {
+    for (let character of characters) {
+        console.log(character.name);
+    }
+})
+
 
 const App = () => {
     return (
-            userServices.getAll().then(value => value.data).then(characters => {
-                for (let character of characters) {
-                    console.log(character.name)
-                }
-            })
+      <div>characterName</div>
 
     );
 };
-
 // 1. Описати всю сім'ю сімпсонів (5 персонажів)
 // 2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
 // https://rickandmortyapi.com/
--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,8 @@ import App from './App';
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
     <App />
-
 );
