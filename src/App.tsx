828a9ce22b0d5fdb8fcc4fdcfd4539a1dab819b9
webader <alexey.derenko@gmail.com>
2023-05-28 15:52:53 +0300

    fix master again

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,3 @@ const App = () => {
 };
 
 export default App;
-
-//1. Описати всю сім'ю сімпсонів (5 персонажів)
-//
-// 2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
-// https://rickandmortyapi.com/
-// https://rickandmortyapi.com/api/character
-// Створити 6 персонажів
